9ecf705dcbe55f1ca5adf4cd07c7fd8f53a515c4
F6089218 Kleber Rodrigues de Freitas <kleber.rodrigues@bb.com.br>
2024-07-22 20:05:28 -0300

    Finalização 7/23

---
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit{
+  @Input() defaultColor: string = 'transparent';
+  @Input('appBetterHighlight') highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+  
+
+  constructor(private renderer: Renderer2, private elRef: ElementRef) { }
+
+
+  ngOnInit() {
+    this.backgroundColor =  this.defaultColor;
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseenter') mouseEnter(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.highlightColor;
+  }
+
+
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = this.defaultColor;
+  }
+
+}
